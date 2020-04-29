c19dda9e60b87fb92ab7bee4766023cbfbeed44b
Marica Antonacci <marica.antonacci@ba.infn.it>
2020-04-29 16:27:58 +0200

    Add deep profile

---
+++ config/deep.py
@@ -0,0 +1,11 @@
+#### LOOK AND FEEL SETTINGS
+WELCOME_MESSAGE = "Welcome to the DEEP PaaS Dashboard!"
+NAVBAR_BRAND_TEXT = "DEEP Orchestrator Dashboard"
+NAVBAR_BRAND_ICON = "/home/static/images/deep/logo.png"
+FAVICON_PATH = "/home/static/images/deep/favicon_io"
+
+### Template Paths
+HOME_TEMPLATE = 'deep/home.html'
+FOOTER_TEMPLATE = 'deep/footer.html'
+
+
