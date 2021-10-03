0bd04a4dc773ffc57ea5f031ec4e10dab62bf4a5
Marica Antonacci <marica.antonacci@ba.infn.it>
2021-10-03 09:07:46 +0200

    Laniakea update (#29) (#30)
    * remove portfolio and enable ssh
    
    * useremail added by default on email type
    
    Co-authored-by: mtangaro <ma.tangaro@gmail.com>
    
    Co-authored-by: mtangaro <ma.tangaro@gmail.com>

--- config/laniakea.py
+++ config/laniakea.py
@@ -3,8 +3,9 @@
 WELCOME_MESSAGE = "Welcome to Laniakea"
 NAVBAR_BRAND_TEXT = "Laniakea Dashboard"
 NAVBAR_BRAND_ICON = "https://raw.githubusercontent.com/Laniakea-elixir-it/resources/master/logos/ELIXIR_ITALY_orange.png"
+### SSH access
+FEATURE_REQUIRE_USER_SSH_PUBKEY = "yes"
 ### Template Paths
 HOME_TEMPLATE = 'laniakea/home.html'
-PORTFOLIO_TEMPLATE = 'laniakea/portfolio.html'
 MAIL_TEMPLATE = 'laniakea/email.html'
 FOOTER_TEMPLATE = 'laniakea/footer.html'
