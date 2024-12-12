7618fbe0cd73d6efa29418bcd9d5290a3df34b41
ettore-infn <147596959+ettore-infn@users.noreply.github.com>
2024-12-12 14:52:38 +0100

    added icsc configuration file, images and created custom home for icsc (#29)

---
+++ config/icsc.py
@@ -0,0 +1,19 @@
+#### LOOK AND FEEL SETTINGS
+WELCOME_MESSAGE = "Welcome to the ICSC Dashboard!"
+NAVBAR_BRAND_TEXT = "Dashboard"
+NAVBAR_BRAND_ICON = "/static/home/images/icsc/icsc-logo-md.png"
+FAVICON_PATH = "/static/home/images/icsc/icsc-logo-sm.ico"
+HELP_PAGE_URL = "https://servicedesk.infn.it"
+BRAND_COLOR_1 = "#bdb1b5"
+BRAND_COLOR_2 = "#0010a0"
+
+FEATURE_REQUIRE_USER_SSH_PUBKEY = "yes"
+FEATURE_PORTS_REQUEST = "yes"
+FEATURE_ACCESS_REQUEST = "no"
+
+### Template Paths
+HOME_TEMPLATE = "icsc/home.html"
+FOOTER_TEMPLATE = "icsc/footer.html"
+
+### Links
+PRIVACY_POLICY_URL = "/"
